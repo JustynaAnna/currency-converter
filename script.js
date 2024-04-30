b4ca82c79c7546e3d0e6d4f01e99796f948f4ba7
JustynaAnna <justynkapawlik@gmail.com>
2024-04-30 22:26:07 +0200

    removed feature setTimeout()

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const exchangeRateInfo = document.querySelector(".exchange-rate-info");
 const baseFlagsBox = document.querySelector(".base-flags-box");
 const targetfetchFlagSvgBox = document.querySelector(".target-flags-box");
 const errorMessage = document.querySelector(".error-message");
-const apiKey = "4444";
+const apiKey = "ae187835fa5da279b910fcd0";
 
 const fetchExchangeRates = async () => {
   const baseCurrency = baseCurrencySelect.value || "PLN";
@@ -17,18 +17,14 @@ const fetchExchangeRates = async () => {
   try {
     // USTAWIENIE LIMITU CZASU DLA ZAPYTANIA
     errorMessage.textContent = "";
-    const currencyData = await fetchDataFromAPI(apiUrl, {
-      timeout: 5000,
-      onTimeout: () => {
-        errorMessage.textContent =
-          "Oops... something went wrong. Please reload the page to try again.";
-      },
-    });
+    const currencyData = await fetchDataFromAPI(apiUrl);
     return { currencyData, baseCurrency, targetCurrency };
   } catch (error) {
     if (error instanceof TypeError && error.message === "Failed to fetch") {
       console.log(error);
-      throw new Error(`Failed to connect to the server`);
+      throw new Error(
+        `Failed to connect to the server. Please check your internet connection and try again.`
+      );
     } else {
       throw new Error(`Failed to fetch exchange rates: ${error.message}`);
     }
@@ -45,12 +41,7 @@ const fetchFlagSvg = async (currency) => {
     }
     const apiURL = `https://restcountries.com/v3.1/currency/${currency}`;
 
-    const flagsSvgData = await fetchDataFromAPI(apiURL, {
-      timeout: 5000,
-      onTimeout: () => {
-        console.log("Timeout occurred while fetching flags data");
-      },
-    });
+    const flagsSvgData = await fetchDataFromAPI(apiURL);
     if (!flagsSvgData[0]?.flags?.svg) {
       throw new Error(`Flag not found for currency ${currency}.`);
     }
@@ -64,26 +55,9 @@ const fetchFlagSvg = async (currency) => {
   }
 };
 
-const fetchDataFromAPI = async (apiUrl, options = {}) => {
-  const { timeout, onTimeout } = options;
-  const controller = new AbortController();
-  const signal = controller.signal;
-
-  // Ustawienie limitu czasu dla zapytania
-  if (timeout) {
-    const timer = setTimeout(() => {
-      controller.abort();
-      if (onTimeout) {
-        onTimeout();
-      }
-    }, timeout);
-
-    signal.addEventListener("abort", () => {
-      clearTimeout(timer);
-    });
-  }
+const fetchDataFromAPI = async (apiUrl) => {
   try {
-    const response = await fetch(apiUrl, { signal });
+    const response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error(`Error fetching exchange rates. ${response.status}`);
     }
@@ -93,15 +67,15 @@ const fetchDataFromAPI = async (apiUrl, options = {}) => {
   }
 };
 
-const calculateExchangeRate = (data, baseCurrency, currency2) => {
-  const rate = data.rates[currency2];
+const calculateExchangeRate = (currencyData, baseCurrency, targetCurrency) => {
+  const rate = currencyData.rates[targetCurrency];
   exchangeRateInfo.textContent = `1 ${baseCurrency} = ${rate.toFixed(
     4
-  )} ${currency2}`;
+  )} ${targetCurrency}`;
   targetAmountInput.value = (baseAmountInput.value * rate).toFixed(2);
 };
 
-const validateInputValue = () => {
+const validateInputAmount = () => {
   const inputValue = baseAmountInput.value;
   const isValid = /^[0-9]*([.,][0-9]*)?$/.test(inputValue);
   const errorMessage = document.querySelector(".input-error");
@@ -114,7 +88,8 @@ const validateInputValue = () => {
   }
 };
 
-const currenciesList = (data, currency2) => {
+//wypełnij opcje walu. Populate = wypełniać
+const populateCurrencySelectOptions = async (currencyData, targetCurrency) => {
   const popularCurrencies = [
     "PLN",
     "EUR",
@@ -126,7 +101,7 @@ const currenciesList = (data, currency2) => {
     "CHF",
   ];
 
-  const currenciesCode = Object.keys(data.rates);
+  const currenciesCode = Object.keys(currencyData.rates);
   const popularGroup = document.createElement("optgroup");
   popularGroup.label = "Popular";
   const otherGroup = document.createElement("optgroup");
@@ -136,32 +111,33 @@ const currenciesList = (data, currency2) => {
   //jak tak to tworzę listę z tych walut
   for (const currencyCode of popularCurrencies) {
     if (currenciesCode.includes(currencyCode)) {
-      const option = document.createElement("option");
-      option.value = currencyCode;
-      option.textContent = currencyCode;
-      popularGroup.appendChild(option);
+      createCurrencyOptionElement(currencyCode, popularGroup);
     }
   }
   // Iteruje po wszystkich kodach walut i dodaje do grupy otherGroup te, które nie należą do popularCurrencies
   for (const currencyCode of currenciesCode) {
     if (!popularCurrencies.includes(currencyCode)) {
-      const option = document.createElement("option");
-      option.value = currencyCode;
-      option.textContent = currencyCode;
-      otherGroup.appendChild(option);
+      createCurrencyOptionElement(currencyCode, otherGroup);
     }
   }
-  // Muszę sklonować, bo przecież nie mogę mieć jednego el. w dwóch miejsach
+  // Muszę sklonować, bo  nie mogę mieć jednego el. w dwóch miejsach :)
   baseCurrencySelect.appendChild(popularGroup);
   baseCurrencySelect.appendChild(otherGroup);
-  targetCurrencySelect.appendChild(popularGroup.cloneNode(true)); // Nie klonujemy popularGroup, ponieważ chcemy, aby była ta sama instancja
+  targetCurrencySelect.appendChild(popularGroup.cloneNode(true));
   targetCurrencySelect.appendChild(otherGroup.cloneNode(true));
 
-  // Ustawienie wartości domyślnej dla targetCurrencySelect na currency2, jeśli base_code to "PLN"
+  // Ustawienie wartości domyślnej dla targetCurrencySelect  na targetCurrency, jeśli base_code to "PLN"
   //inaczej mam PLN PLN
-  if (data.base_code === "PLN") {
-    targetCurrencySelect.value = currency2;
+  if (currencyData.base_code === "PLN") {
+    targetCurrencySelect.value = targetCurrency;
   }
+};
+
+const createCurrencyOptionElement = (currencyCode, element) => {
+  const option = document.createElement("option");
+  option.value = currencyCode;
+  option.textContent = currencyCode;
+  element.appendChild(option);
 };
 
 const swap = () => {
@@ -169,64 +145,48 @@ const swap = () => {
     targetCurrencySelect.value,
     baseCurrencySelect.value,
   ]; // destrukturyzacja pozwala na zmianę miejsca wartości
-  fetchExchangeData();
+  fetchExchangeRates();
 };
 
-const handleExchange = async () => {
+const displayCountryFlags = async (baseCurrency, targetCurrency) => {
+  const updateFlag = async (currency, className, container) => {
+    let imgTag = document.querySelector(`.${className}`);
+    if (!imgTag) {
+      imgTag = document.createElement("img");
+      imgTag.className = className;
+      imgTag.style.width = "42px";
+      container.appendChild(imgTag);
+    }
+    const flagURL = await fetchFlagSvg(currency);
+    if (flagURL) {
+      imgTag.src = flagURL;
+    }
+  };
+
+  await updateFlag(baseCurrency, "flag-img-base", baseFlagsBox);
+  await updateFlag(
+    targetCurrency,
+    "flag-img-targetCurrency",
+    targetfetchFlagSvgBox
+  );
+};
+
+const updateExchangeDetails = async () => {
   try {
-    const { data, baseCurrency, currency2 } = await fetchExchangeData();
-
-    // Sprawdź, czy tag img dla baseCurrency już istnieje
-    let imgTagBaseCurrency = document.querySelector(".flag-img-base");
-    if (imgTagBaseCurrency) {
-      // Jeśli istnieje, zaktualizuj jego src
-      const flagURLBaseCurrency = await getFlags(baseCurrency);
-      if (flagURLBaseCurrency) {
-        imgTagBaseCurrency.src = flagURLBaseCurrency;
-      }
-    } else {
-      // Jeśli nie istnieje, stwórz nowy tag img dla baseCurrency
-      imgTagBaseCurrency = document.createElement("img");
-      imgTagBaseCurrency.className = "flag-img-base"; // Dodaj klasę, aby łatwo go znaleźć
-      const flagURLBaseCurrency = await getFlags(baseCurrency);
-      if (flagURLBaseCurrency) {
-        imgTagBaseCurrency.src = flagURLBaseCurrency;
-        imgTagBaseCurrency.style.width = "52px";
-        baseFlagsBox.appendChild(imgTagBaseCurrency);
-      }
-    }
-
-    // Sprawdź, czy tag img dla currency2 już istnieje
-    let imgTagCurrency2 = document.querySelector(".flag-img-currency2");
-    if (imgTagCurrency2) {
-      // Jeśli istnieje, zaktualizuj jego src
-      const flagURLCurrency2 = await getFlags(currency2);
-      if (flagURLCurrency2) {
-        imgTagCurrency2.src = flagURLCurrency2;
-      }
-    } else {
-      // Jeśli nie istnieje, stwórz nowy tag img dla currency2
-      imgTagCurrency2 = document.createElement("img");
-      imgTagCurrency2.className = "flag-img-currency2"; // Dodaj klasę, aby łatwo go znaleźć
-      const flagURLCurrency2 = await getFlags(currency2);
-      if (flagURLCurrency2) {
-        imgTagCurrency2.src = flagURLCurrency2;
-        imgTagCurrency2.style.width = "52px";
-        targetfetchFlagSvgBox.appendChild(imgTagCurrency2);
-      }
-    }
-
-    currenciesList(data, currency2);
-    calculateExchangeRate(data, baseCurrency, currency2);
-    validateInputValue();
+    const { currencyData, baseCurrency, targetCurrency } =
+      await fetchExchangeRates();
+    displayCountryFlags(baseCurrency, targetCurrency);
+    populateCurrencySelectOptions(currencyData, targetCurrency);
+    calculateExchangeRate(currencyData, baseCurrency, targetCurrency);
+    validateInputAmount();
   } catch (error) {
     const errorMessage = document.querySelector(".error-message");
     errorMessage.textContent = error.message;
   }
 };
 
-baseCurrencySelect.addEventListener("change", handleExchange);
-targetCurrencySelect.addEventListener("change", handleExchange);
-baseAmountInput.addEventListener("input", handleExchange);
+baseCurrencySelect.addEventListener("change", updateExchangeDetails);
+targetCurrencySelect.addEventListener("change", updateExchangeDetails);
+baseAmountInput.addEventListener("input", updateExchangeDetails);
 swapBtn.addEventListener("click", swap);
-handleExchange();
+updateExchangeDetails();
